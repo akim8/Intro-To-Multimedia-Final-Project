5e1711ee3c81f449e56a38829089228e06ec6e0a
Alex Kim <kim4a@cmich.edu>
2017-12-04 23:39:46 -0500

    add hide unless player close and audio script and bgm + sfx

---
+++ Intro to Multimedia Final Project/Assets/Scripts/HideUnlessNear.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideUnlessNear : MonoBehaviour {
+	public GameObject playerObject;
+	public float distance = 15f;
+	public bool playSound = true;
+	public AudioClip soundClip;
+	private AudioSource source;
+	private bool alreadyPlayed = false;
+
+	// Use this for initialization
+	void Start () {
+		if (playerObject == null && GameObject.Find ("Player Controller") != null)
+			playerObject = GameObject.Find ("Player Controller");
+		
+		if (playSound) {
+			if (soundClip != null)
+				source = GetComponent<AudioSource> ();
+			else
+				Debug.Log ("No sound clip selected to play!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerObject != null) {
+			var getDistance = Vector3.Distance (transform.position, playerObject.transform.position);
+			if (getDistance < distance && GetComponentInChildren<Renderer> ().enabled == false) {
+				GetComponentInChildren<Renderer> ().enabled = true;
+
+				if (playSound) {
+					if (soundClip != null && !alreadyPlayed) {
+						source.PlayOneShot (soundClip);
+						alreadyPlayed = true;
+					}
+				}
+			}
+			else if (getDistance >= distance && GetComponentInChildren<Renderer> ().enabled == true) {
+				GetComponentInChildren<Renderer> ().enabled = false;
+				alreadyPlayed = false;
+			}
+		}
+	}
+}
+
